76b3ff0b1c2806aefd9df12a9910b80502c71b05
rgomide <renato.s.gomide@gmail.com>
2025-12-12 11:35:34 -0300

    .

---
+++ frontend/src/components/TopBar/TopBar.stories.js
@@ -0,0 +1,46 @@
+import TopBar from './TopBar.vue'
+import { useAuthStore } from '@/stores/storeAuth'
+
+export default {
+  title: 'Components/TopBar',
+  component: TopBar
+}
+
+const TopBarTemplate = `
+    <TopBar />
+`
+
+export const Default = {
+  name: 'User is logged in',
+  args: {},
+  render: (args) => ({
+    components: { TopBar },
+    setup() {
+      const authStore = useAuthStore()
+      authStore.setAuthenticatedUser({
+        id: 1,
+        name: 'John Doe',
+        email: 'john.doe@example.com',
+        roles: ['admin']
+      })
+      authStore.setAuthenticatedToken('1234567890')
+      return { args }
+    },
+    template: TopBarTemplate
+  })
+}
+
+export const UserIsNotLoggedIn = {
+  name: 'User is not logged in',
+  args: {},
+  render: (args) => ({
+    components: { TopBar },
+    setup() {
+      const authStore = useAuthStore()
+      authStore.setAuthenticatedUser(null)
+      authStore.setAuthenticatedToken(null)
+      return { args }
+    },
+    template: TopBarTemplate
+  })
+}
--- frontend/src/views/AuthView/AuthView.stories.js
+++ frontend/src/views/AuthView/AuthView.stories.js
@@ -1,13 +1,37 @@
 import AuthView from './AuthView.vue'
 import { vueRouter } from 'storybook-vue3-router'
+import { http, HttpResponse, delay } from 'msw'
 
 export default {
   title: 'Views/AuthView',
   component: AuthView
 }
 
+const onShowMessage = (message) => {
+  console.log(message)
+}
+
+const handlers = [
+  http.post('*/api/auth/login', async ({ request }) => {
+    return HttpResponse.json({ token: '1234567890' }, { status: 200 })
+  })
+]
+
+const handlersError = [
+  http.post('*/api/auth/login', async ({ request }) => {
+    const body = await request.json()
+    await delay(500)
+    return HttpResponse.json(
+      {
+        message: 'Forbidden'
+      },
+      { status: 403 }
+    )
+  })
+]
+
 const AuthViewTemplate = `
-    <AuthView @showAlert="args.showAlert" />
+    <AuthView @showMessage="args.showMessage" />
 `
 
 export const Default = {
@@ -20,5 +44,27 @@ export const Default = {
     },
     template: AuthViewTemplate
   }),
-  decorators: [vueRouter()]
+  decorators: [vueRouter()],
+  parameters: { msw: { handlers } }
+}
+
+
+export const Error = {
+  name: 'Auth error',
+  args: {
+    showMessage: onShowMessage
+  },
+  render: (args) => ({
+    components: { AuthView },
+    setup() {
+      return { args }
+    },
+    template: AuthViewTemplate
+  }),
+  decorators: [vueRouter()],
+  parameters: {
+    msw: {
+      handlers: handlersError
+    }
+  }
 }
